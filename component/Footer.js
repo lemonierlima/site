50c365db51d05842cbf704589e378e84c9687ac2
Lemonier Lima <lemonier.lima@gmail.com>
2022-04-12 00:16:05 -0300

    repaginada 2

--- component/Footer.js
+++ component/Footer.js
@@ -2,9 +2,8 @@ import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
   return (
-  <nav className={styles.footer} 
-    class="navbar sticlassName={styles.footer} cky-top navbar-light bg-light">
+  <div class="navbar fixed-bottom">
     <span className={styles.span}>LL</span> &copy; 2021 
-  </nav>
+  </div>
   )
 }
